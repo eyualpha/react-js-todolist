ca7b361f0915ef72d90c26375638c8cc58fb6772
eyualpha <eyuashenafi533@gmail.com>
2024-11-13 23:06:31 +0300

    removed componests

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import TodoInput from "./components/TodoInput";
-import TodoCard from "./components/TodoCard";
 import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <div className="app">
-      <TodoInput />
       <TodoList />
     </div>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
 const TodoList = () => {
-  let todos = [
+  const [todos, setTodos] = React.useState([
     "Learn React",
     "Learn Node",
     "Learn Express",
     "Learn MongoDB",
     "Learn Redux",
-  ];
+  ]);
+
+  function addTodo() {
+    setTodos([...todos, "Learn React"]);
+  }
   return (
     <div>
+      <div className="input-section">
+        <input
+          onChange={setValue(value)}
+          value={txt}
+          type="text"
+          placeholder="Enter a todo..."
+        />
+        <button onClick={() => addTodo()}>Add Todo</button>
+      </div>
+
       <ol>
         {todos.map((todo, index) => {
           return (
